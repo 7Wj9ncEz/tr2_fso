077d4bf2dd36c536f29d9f066b17eeb2717263e3
Vinicius Bandeira <bandeiravinicius0@live.com>
2017-10-13 21:30:55 -0300

    add numero de travessias e estado cansado

--- semaphore.c
+++ semaphore.c
@@ -8,22 +8,27 @@
 #include <sys/sem.h>
 
 #define SEM_KEY		0x1234
-#define BRINCANDO 1
+#define PENSANDO 1
 #define ESPERANDO 2
 #define ATRAVESSANDO 3
+#define CANSADO 4
 
-int		g_sem_id;
+int		g_sem_id, count;
 
 struct sembuf	g_lock_sembuf[1];
 struct sembuf	g_unlock_sembuf[1];
 
-void liga_crianca(int lado, int id) {
-	int estado = BRINCANDO;
+void retira_crianca(){
+	count -= 1;
+}
+
+void liga_crianca(int lado, int id, int num_travessias) {
+	int estado = PENSANDO;
 	sleep(2);
 	while(1) {
-		if(estado == BRINCANDO) {
+		if(estado == PENSANDO) {
 
-			printf("Crianca %d: BRINCANDO do lado %d\n", id,lado);
+			printf("Crianca %d: PENSANDO do lado %d\n", id,lado);
 			fflush(stdout);
 
 			srand( (unsigned)time(NULL) + id );
@@ -42,11 +47,21 @@ void liga_crianca(int lado, int id) {
 		if(estado == ATRAVESSANDO) {
 			printf("Crianca %d: ATRAVESSANDO\n", id);
 			fflush(stdout);
-			sleep(2);
-			semop (g_sem_id, g_unlock_sembuf, 1);
+			usleep(70);
+			num_travessias-=1;
 			printf("Crianca %d: Atravessei\n", id);
+			semop (g_sem_id, g_unlock_sembuf, 1);
 			fflush(stdout);
-			estado = BRINCANDO;
+			if(num_travessias>0){
+				estado = PENSANDO;
+			}else{
+				estado = CANSADO;
+			}
+		}
+		if(estado == CANSADO) {
+			kill(getppid(), SIGUSR1);
+			printf("Crianca %d: Cansei, vou pra casa\n", id);
+			exit(1);
 		}
 	}
 }
@@ -54,11 +69,15 @@ void liga_crianca(int lado, int id) {
 int main() {
 	int num_criancas_dir = 0;
 	int num_criancas_esq = 0;
+	int num_travessias = 0;
 	printf("Digite a quantidade de criancas: ESQUERDA DIREITA\n");
 	scanf("%d %d", &num_criancas_esq, &num_criancas_dir);
 
+	printf("Digite o numero de travesias:");
+	scanf("%d", &num_travessias);
 
-	int rtn, count;
+
+	int rtn;
 	int * pid = malloc(sizeof(int)*(num_criancas_esq+num_criancas_dir));
 
 	/* Construindo a estrutura de controle do semaforo */
@@ -86,16 +105,16 @@ int main() {
 		/* Estou no processo filho... */
 		printf("Filho %i comecou ...\n", count);
 		fflush(stdout);
-		liga_crianca(count%2, count);
+		liga_crianca(count%2, count, num_travessias);
   	} 
   	else {
+		  signal(SIGUSR1,retira_crianca);
 		/* Estou no processo pai ... */
-		sleep(200);
-		/* Matando os processos filhos  */
-		for(int i = 0; i < num_criancas_esq + num_criancas_dir; i++) 
-		{
-			kill(pid[i], SIGTERM);
+		//esperando ate todas as crianças se cansarem
+		while(count>0){
+			sleep(1);
 		}
+		printf("TODAS AS CRIANÇAS SE CANSARAM E FORAM EMBORA.\n");
 		/* Removendo o semaforo */
 		semctl(g_sem_id, 0, IPC_RMID, 0);
   	} /* fim-else */
